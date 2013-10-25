85d619591e9d42d7f8255ed376d752e5174e5136
nate prewitt <nate.prewitt@gmail.com>
2013-10-24 23:55:52 -0600

    added animations, cleaner menu selection, mediocre animations

--- js/produkt.js
+++ js/produkt.js
@@ -11,3 +11,12 @@ function github()
     document.getElementById("github").style.diplay="block";
     document.getElementById("bannertitle").style.display="none";
 }
+function infoslide()
+{ 
+    //document.getElementById("aboutcontainer").style.display="none";
+    //document.getElementById("resumecontainer").style.display="none";
+    //document.getElementById("gitcontainer").style.display="none";
+    //document.getElementById("bannertitle").style.display="block";
+    //document.getElementById("gitimg").style.display="none";
+    //document.getElementById("contactcontainer").style.display="none";
+}
