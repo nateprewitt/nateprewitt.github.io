4fd2059aaf55a439d26aa3ec3643982d6a8ebcfa
Nate <Nate@semanticarts.com>
2013-10-25 18:10:42 -0600

    added flask setup

---
+++ templates/js/produkt.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+    (function setswitcher(){
+        var anchor = window.location.hash;
+        contentswitch(anchor);
+    })();
+    function contentswitch(id){    
+
+        //setTimeout($('.active').addClass('.out'), 1200);
+        $('.active').hide();
+        $('.active').removeClass('active');
+        if (id !== "") {
+            $(id+"container").addClass('active'); 
+            $(id+"container").show();
+        }
+        else {
+            $('#homecontainer').addClass('active');
+            $('#homecontainer').show();
+        }
+    }
+    $(document).on('click', '.box', function(){
+        $this = $(this);
+        var id = $this.attr('id');
+        contentswitch('#'+id); //appending # for future selector use
+    });
+});
